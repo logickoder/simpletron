3b714aaa81431069f7b09cc4f188bc905c6b40ab
Jeffrey Orazulike <chukwudumebiorazulike@gmail.com>
2021-12-25 08:48:55 +0100

    Added a default name for registers

--- core/src/main/java/com/jeffreyorazulike/simpletron/core/components/Register.kt
+++ core/src/main/java/com/jeffreyorazulike/simpletron/core/components/Register.kt
@@ -5,16 +5,15 @@ package com.jeffreyorazulike.simpletron.core.components
  * @author Jeffrey Orazulike [chukwudumebiorazulike@gmail.com]
  * Created on 21 at 4:03 AM
  *
- * Defines a register in the [CPU]
+ * A component in the [CPU] that stores values from the [Memory]
+ * and execution of [Instruction]s so that they can be easily accessed by the [CPU]
+ *
+ * @property value the current word the register holds
+ * @property name the name of the register
  */
 abstract class Register {
-    /**
-     * The value of the register
-     * */
+
     var value: Int = 0
 
-    /**
-     * The name of the register
-     * */
-    abstract val name: String
+    open val name: String = this::class.simpleName.toString()
 }
--- core/src/main/java/com/jeffreyorazulike/simpletron/core/components/Registers.kt
+++ core/src/main/java/com/jeffreyorazulike/simpletron/core/components/Registers.kt
@@ -3,38 +3,28 @@ package com.jeffreyorazulike.simpletron.core.components
 /**
  * Holds the calculation the CPU is currently processing
  */
-object Accumulator: Register(){
-    override val name: String = this::class.simpleName.toString()
-}
+object Accumulator: Register()
 
 /**
  * Holds the operation the CPU is to perform
  */
-object OperationCode: Register(){
-    override val name: String = this::class.simpleName.toString()
-}
+object OperationCode: Register()
 
 /**
  * Holds the value the CPU is to use the [OperationCode] and work on
  */
-object Operand: Register(){
-    override val name: String = this::class.simpleName.toString()
-}
+object Operand: Register()
 
 /**
  * Holds the current memory location the CPU should get what to work on
  */
-object InstructionCounter: Register(){
-    override val name: String = this::class.simpleName.toString()
-}
+object InstructionCounter: Register()
 
 /**
  * Holds the value of the memory location gotten from the [InstructionCounter] to
  * prevent unnecessary reading from the memory
  */
-object InstructionRegister: Register(){
-    override val name: String = this::class.simpleName.toString()
-}
+object InstructionRegister: Register()
 
 fun defaultRegisters() = listOf(
     Accumulator, OperationCode, Operand, InstructionCounter, InstructionRegister
