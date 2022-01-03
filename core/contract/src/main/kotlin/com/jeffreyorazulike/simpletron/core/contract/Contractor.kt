558128433451efc3d44db762e844da091f6fd830
Jeffrey Orazulike <chukwudumebiorazulike@gmail.com>
2022-01-03 16:30:52 +0100

    [chore] moved contracts to another module

---
+++ core/contract/src/main/kotlin/com/jeffreyorazulike/simpletron/core/contract/Contractor.kt
@@ -0,0 +1,10 @@
+package com.jeffreyorazulike.simpletron.core.contract
+
+/**
+ *
+ * @author Jeffrey Orazulike [chukwudumebiorazulike@gmail.com]
+ * Created on 26 at 9:33 PM
+ *
+ * A [Contract] executor
+ */
+interface Contractor {}
