6838f5653be144e8dc8bf38a5977bd47ff84097b
Jeffrey Orazulike <chukwudumebiorazulike@gmail.com>
2022-01-05 14:02:39 +0100

    [feat] added maven-publish plugin for jitpack publishing

--- core/impl/src/main/kotlin/com/jeffreyorazulike/simpletron/core/impl/utils/ComponentUtils.kt
+++ core/impl/src/main/kotlin/com/jeffreyorazulike/simpletron/core/impl/utils/ComponentUtils.kt
@@ -17,10 +17,8 @@ inline fun <reified T> classInstances(classes: Set<Class<*>>): List<T> {
     // retrieve all the required classes defined in the packages of the param [classes]
     return classes.asSequence().map { clazz ->
         Reflections(clazz.packageName).getSubTypesOf(T::class.java)
-    }.reduce { acc, otherClasses ->
         // merge all into one set
-        acc.union(otherClasses)
-    }.map {
+    }.reduce { acc, otherClasses -> acc.union(otherClasses) }.map {
         // return instances of each of them
         it.kotlin.objectInstance ?: it.getDeclaredConstructor().newInstance()
     }
