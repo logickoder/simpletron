2ffacf2163a930e6db24e6c0cebbf164388ddd66
Jeffrey Orazulike <chukwudumebiorazulike@gmail.com>
2022-01-13 23:55:32 +0100

    [feat] created the utils module

---
+++ utils/src/main/kotlin/com/logickoder/simpletron/core/utils/ClassUtils.kt
@@ -0,0 +1,18 @@
+package com.logickoder.simpletron.core.utils
+
+import org.reflections.Reflections
+
+typealias Classes<T> = List<Class<out T>>
+
+/**
+ * Returns the classes that are subtypes of the generic type in the given packages
+ *
+ * @param packages set of classes to search for the given type in their package
+ * */
+inline fun <reified T> classes(packages: Set<Class<*>>): Classes<T> {
+    // retrieve all the required classes defined in the packages of the param [classes]
+    return packages.asSequence().map { clazz ->
+        Reflections(clazz.packageName).getSubTypesOf(T::class.java)
+        // merge all into one set
+    }.reduce { acc, otherClasses -> acc.union(otherClasses) }.toList()
+}
